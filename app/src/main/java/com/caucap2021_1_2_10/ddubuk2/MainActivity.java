082746d6a0ba91faa5ace2a721bb548cde72ea08
Mesopotamia2957 <mesopotamia2957@gmail.com>
2021-10-09 15:42:00 +0900

    Initial commit

---
+++ app/src/main/java/com/caucap2021_1_2_10/ddubuk2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.caucap2021_1_2_10.ddubuk2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
